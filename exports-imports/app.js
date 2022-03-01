72ad48fc348aac7dd6abcb97819102e903128c2d
manu-yaff <manuelyg2097@gmail.com>
2022-02-28 21:51:09 -0600

    add some really basic examples of modern js

---
+++ exports-imports/app.js
@@ -0,0 +1,8 @@
+import person from './person.js';
+import prs from './person.js';
+
+import { baseData } from './utility.js';
+import { clean } from './utility.js';
+
+// imports everything
+import * as utilities from './utility';
---
+++ exports-imports/utility.js
@@ -0,0 +1,2 @@
+export const clean = () => 'hello world';
+export const baseData = () => 10;
